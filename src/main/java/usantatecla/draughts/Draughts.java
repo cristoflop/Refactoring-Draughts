1f2ec040d912d1a1bfaa742ae3b5603f878a3e6f
cristoflop <cristofer.lc.99@gmail.com>
2020-11-29 14:32:48 +0100

    Refactor main class

--- src/main/java/usantatecla/draughts/Draughts.java
+++ src/main/java/usantatecla/draughts/Draughts.java
@@ -5,27 +5,24 @@ import usantatecla.draughts.controllers.InteractorController;
 import usantatecla.draughts.views.View;
 
 class Draughts {
-    
-    private View view;
 
     private Logic logic;
 
-    private Draughts(){
-        this.view = new View();
+    private Draughts() {
         this.logic = new Logic();
     }
 
     private void play() {
         InteractorController controller;
-		do {
-			controller = this.logic.getController();
-			if (controller != null)
-				this.view.interact(controller);
-		} while (controller != null); 
+        do {
+            controller = this.logic.getController();
+            if (controller != null)
+                controller.control();
+        } while (controller != null);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Draughts().play();
     }
-    
+
 }
