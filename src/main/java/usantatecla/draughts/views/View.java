b07fcf13c42ad343817414e0edc62d3ca7145baa
cristoflop <cristofer.lc.99@gmail.com>
2020-11-29 12:00:35 +0100

    Merged StartView in View

--- src/main/java/usantatecla/draughts/views/View.java
+++ src/main/java/usantatecla/draughts/views/View.java
@@ -1,19 +1,18 @@
 package usantatecla.draughts.views;
 
-import usantatecla.draughts.controllers.InteractorController;
-import usantatecla.draughts.controllers.InteractorControllersVisitor;
-import usantatecla.draughts.controllers.PlayController;
-import usantatecla.draughts.controllers.ResumeController;
-import usantatecla.draughts.controllers.StartController;
+import usantatecla.draughts.controllers.*;
 
-public class View implements InteractorControllersVisitor {
+public class View extends SubView implements InteractorControllersVisitor {
 
-    private StartView startView;
+    private static final String TITTLE = "Draughts";
+
+    // private StartView startView;
     private PlayView playView;
     private ResumeView resumeView;
 
-    public View(){
-        this.startView = new StartView();
+    public View() {
+        super();
+        // this.startView = new StartView();
         this.playView = new PlayView();
         this.resumeView = new ResumeView();
     }
@@ -23,10 +22,17 @@ public class View implements InteractorControllersVisitor {
         controller.accept(this);
     }
 
+    void interact(StartController startController) {
+        assert startController != null;
+        this.console.writeln(this.TITTLE);
+        new GameView().write(startController);
+        startController.start();
+    }
+
     @Override
     public void visit(StartController startController) {
         assert startController != null;
-        this.startView.interact(startController);
+        this.interact(startController);
     }
 
     @Override
